dda42693b8eb8cf0b55f33798e8c0f11f9cffe75
akameco <akameco.t@gmail.com>
2017-07-30 07:43:58 +0900

    Resizable Video Player

--- components/DemoVideo.js
+++ components/DemoVideo.js
@@ -1,16 +1,49 @@
 // @flow
 import React, { Component } from 'react'
+import EventListener from 'react-event-listener'
 import Youtube from 'react-youtube'
 
+type State = {
+  width: number,
+  height: number,
+}
+
+const defaultWidth = 730
+const defaultHeight = 500
+
 export default class DemoVideo extends Component {
+  state: State = {
+    width: defaultWidth,
+    height: defaultHeight,
+  }
+
   handleOnReady = (event: any) => {
     event.target.setVolume(0)
   }
 
+  handleResize = () => {
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+
+    if (windowWidth < defaultWidth && windowWidth > 200) {
+      this.setState({ width: windowWidth - 10 })
+    } else {
+      this.setState({ width: defaultWidth })
+    }
+
+    if (windowHeight < defaultHeight && windowHeight > 200) {
+      this.setState({ height: windowHeight - 10 })
+    } else {
+      this.setState({ height: defaultHeight })
+    }
+  }
+
   render() {
+    const { width, height } = this.state
+
     const youtubeOpts = {
-      width: '730',
-      height: '500',
+      width,
+      height,
       playerVars: {
         rel: 0,
         autoplay: 1,
@@ -19,11 +52,14 @@ export default class DemoVideo extends Component {
     }
 
     return (
-      <Youtube
-        videoId="DsL9NC8cWVE"
-        opts={youtubeOpts}
-        onReady={this.handleOnReady}
-      />
+      <div>
+        <EventListener target="window" onResize={this.handleResize} />
+        <Youtube
+          videoId="DsL9NC8cWVE"
+          opts={youtubeOpts}
+          onReady={this.handleOnReady}
+        />
+      </div>
     )
   }
 }
