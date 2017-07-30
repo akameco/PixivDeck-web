ee6edd12c936763924cf11bea83450ff9ab3651c
akameco <akameco.t@gmail.com>
2017-07-30 22:27:13 +0900

    Update Header

--- components/Header.js
+++ components/Header.js
@@ -7,22 +7,16 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <div className="container">
-          <div className="logo">
-            <Center direction="row">
-              <Logo />
-              <h1>PixivDeck</h1>
-            </Center>
-          </div>
+        <div className="logo">
+          <Center direction="row">
+            <Logo />
+            <h1>PixivDeck</h1>
+          </Center>
         </div>
         <style jsx>{`
           header {
             width: 100%;
             background: black;
-          }
-          .container {
-            margin: 0 auto;
-            max-width: 760px;
           }
           .logo {
             padding: 0 20px;
