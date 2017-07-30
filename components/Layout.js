257b2ce7d0040db2989523767b56a88c6e0a88f5
akameco <akameco.t@gmail.com>
2017-07-30 12:50:59 +0900

    Update OPG setting

--- components/Layout.js
+++ components/Layout.js
@@ -17,19 +17,35 @@ export default class Layout extends React.Component {
           <title>Home page</title>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content="next.jsを使った静的サイト。OGP対応確認中" />
-
-          <meta property="og:title" content="Home Page" />
+          <meta
+            property="og:title"
+            content="PixivDeck: TweetDeckライクな高機能pixivクライアント"
+          />
           <meta
             property="og:url"
-            content="https://akameco.github.io/demo-next-static-export"
+            content="https://akameco.github.io/PixivDeck-web"
           />
           <meta property="og:site_name" content="https://akameco.github.io" />
-          <meta property="og:description" content="next.jsを使った静的サイトです。" />
 
-          <meta property="fb:app_id" content="456581931407339" />
+          <meta
+            name="description"
+            content="PixivDeck: TweetDeckライクな高機能pixivクライアント。無料ダウンロード。カラムベースでたくさんのイラストを一度に閲覧。ワンクリックブックマーク。ブックマークフィルタ。新着のデスクトップ通知。閲覧履歴等多くの機能がサポートされてます。"
+          />
 
-          <meta name="twitter:card" content="summary" />
+          <meta
+            property="og:description"
+            content="PixivDeck: TweetDeckライクな高機能pixivクライアント。無料ダウンロード。カラムベースでたくさんのイラストを一度に閲覧。ワンクリックブックマーク。ブックマークフィルタ。新着のデスクトップ通知。閲覧履歴等多くの機能がサポートされてます。"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="https://akameco.github.io/PixivDeck-web/static/ogp.png"
+          />
+
+          <meta property="fb:app_id" content="161451777749561" />
+
+          <meta name="twitter:card" content="photo" />
           <meta name="twitter:site" content="@akameco" />
 
           <link
