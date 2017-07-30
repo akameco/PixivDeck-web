c3fee99711ebba9fb547b3467a44f82d72a7ee1e
akameco <akameco.t@gmail.com>
2017-07-30 13:07:17 +0900

    Move Title Component

--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,8 @@ import Header from '../components/Header'
 import Container from '../components/Container'
 import Download from '../components/Download'
 import Center from '../components/Center'
-import Logo from '../components/Logo'
+import Title from '../components/Title'
 import DemoVideo from '../components/DemoVideo'
-
-class Title extends Component {
-  render() {
-    return (
-      <div className="title">
-        <Center direction="row">
-          <Logo />
-          <h1>PixivDeck</h1>
-        </Center>
-        <h2>TweetDeckライクな高機能pixivクライアント</h2>
-        <style jsx>{`
-          .title {
-            margin-top: 15px;
-            margin-bottom: 30px;
-          }
-
-          h1 {
-            font-size: 48px;
-            margin-left: 5px;
-          }
-
-          h2 {
-            font-size: 24px;
-            color: #5a5252;
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
 
 const staticPath = process.env.STATIC_PATH || '/static'
 
